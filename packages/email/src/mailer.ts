ddfc17b9c2a5f303a998e7fbd0f0a1e74bf13a1a
aryan <aryanak9163@gmail.com>
2026-09-04 20:18:54 +0530

    feat: adds nodemailer transporter config

--- packages/email/src/mailer.ts
+++ packages/email/src/mailer.ts
@@ -1,1 +1,10 @@
-console.log("Welcome to the mailer client")
+import "dotenv/config";
+import nodemailer from "nodemailer";
+
+export const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+    }
+});
